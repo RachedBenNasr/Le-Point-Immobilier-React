8efd4202e6a0784f45ab3818c05782f67701fe2f
RachedBenNasr <110614420+RachedBenNasr@users.noreply.github.com>
2023-09-04 15:28:40 +0100

    Initial push to zen

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './style.css'
-import ComingSoon from './views/coming-soon'
-import Home from './views/home'
-import NotFound from './views/not-found'
-import Promote from './views/promote'
-import Buying from './views/buying'
-import Publish from './views/publish'
-import Contact from './views/contact'
-import Services from './views/services'
-import Renting from './views/renting'
-import Commercial from './views/commercial'
-import Opportunities from './views/opportunities'
+import "./style.css";
+import ComingSoon from "./views/coming-soon";
+import Home from "./views/home";
+import NotFound from "./views/not-found";
+import Promote from "./views/promote";
+import Buying from "./views/buying";
+import Publish from "./views/publish";
+import Contact from "./views/contact";
+import Services from "./views/services";
+import Renting from "./views/renting";
+import Commercial from "./views/commercial";
+import Opportunities from "./views/opportunities";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Route component={ComingSoon} exact path="/soon" />
-        <Route component={Home} exact path="/home" />
-        <Route component={NotFound} path="**" />
+        <Route component={ComingSoon} exact path="/**" />
+        {/* <Route component={Home} exact path="/home" />
+         <Route component={NotFound} path="**" /> 
         <Route component={Promote} exact path="/promote" />
         <Route component={Buying} exact path="/buying" />
         <Route component={Publish} exact path="/publish" />
@@ -29,10 +29,10 @@ const App = () => {
         <Route component={Services} exact path="/services" />
         <Route component={Renting} exact path="/renting" />
         <Route component={Commercial} exact path="/commercial" />
-        <Route component={Opportunities} exact path="/opportunities" />
+        <Route component={Opportunities} exact path="/opportunities" /> */}
       </div>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
